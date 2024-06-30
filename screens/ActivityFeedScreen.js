b06c081487328e1ccd9149dcd060fc2e5974116d
AndreaCoa <andrea.coa@utec.edu.pe>
2024-06-30 14:39:54 -0500

    basic cloudinary setup

--- screens/ActivityFeedScreen.js
+++ screens/ActivityFeedScreen.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { act, useEffect, useState } from 'react'
+import { FlatList, View , Text, ImageBackground, TouchableHighlight, Button} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { fetchActivityInProgress, getRoleBasedOnToken } from '../api';
+import {useNavigation} from '@react-navigation/native';
+
 
 export const ActivityFeedScreen = () => {
+  const [activities, setActivities] = useState([]);
+  const [page, setPage] = useState(0);
+  const role = getRoleBasedOnToken();
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const getActivities = async(page) => {
+      try {
+        const res = await fetchActivityInProgress(page);
+        setActivities(res.content);
+      }
+      catch(error) {
+        console.error('failed to fetch activities', error);
+      }
+    }
+    getActivities(page);
+  }, [page]);
+  console.log(activities);
+
   return (
-    <div>ActivityFeedScreen</div>
+    <ImageBackground source={require("../assets/dog_bg.png")} style={{ flex: 1 }}>
+      {role === 'ROLE_COMPANY' && (
+      <Button 
+        title="Crear Actividad"
+        accessibilityLabel="Botón para crear nueva actividad"
+      />
+    )}
+    <SafeAreaView>    
+      <FlatList
+        data ={activities}
+        keyExtractor={(item)=> item.id.toString()}
+        // ListHeaderComponent={<Text>Eventos disponibles</Text>}
+        renderItem={({item}) => (
+          <TouchableHighlight onPress={() => {console.log("jolaaa", item.id); navigation.navigate('Evento', { id: item.id })}}>
+            <View>
+              <Text> {item.name}</Text>
+              <Text>{item.activityType}</Text>
+              <Text>{item.companyDto.name}</Text>
+              <Text>{item.startDate}</Text>
+              <Text>{item.endDate}</Text>
+              <Text>{item.locations && item.locations.length > 0 ? item.locations[0].address : 'No location available'}</Text>
+            </View>
+          </TouchableHighlight>
+        )}
+
+      />
+    </SafeAreaView>
+    </ImageBackground>
+
   )
 }
--- screens/ActivityScreen.js
+++ screens/ActivityScreen.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import {fetchSingleActivity} from '../api';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View, Image} from 'react-native';
+import {cld} from '../Cloudinary'
 
-export const ActivityScreen = () => {
+
+
+
+export const ActivityScreen = (props) => {
+  const [activity,  setActivity] = useState(null);
+
+  
+  useEffect(()=> {
+    const getActivity = async() => {
+      try {
+        const res = await fetchSingleActivity(props.route.params.id);
+        console.log("IIIII", res);
+        setActivity(res);
+
+      }
+      catch(error) {
+        console.error('Error fetching activity', error);
+      }
+    }
+    getActivity();
+  }, [props.route.params.id]);
+
+  console.log("Maria");
+
+
   return (
-    <div>ActivityScreen</div>
-  )
+    <SafeAreaView >
+      {activity && 
+        <View>
+          <Image 
+          source={{uri:"https://res.cloudinary.com/dp7zuvv8c/image/upload/v1/PetPals/nbgzcrq0gafkkiafmkeq?_a=DATAdtAAZAA0"}}
+          style={{width:300, height:300}}
+          resizeMode='cover'
+          />
+          <Text> {activity.name}</Text>
+              <Text>{activity.activityType}</Text>
+              <Text>{activity.companyDto.name}</Text>
+              <Text>{activity.startDate}</Text>
+              <Text>{activity.endDate}</Text>
+              <Text>{activity.locations && activity.locations.length > 0 ? activity.locations[0].address : 'No location available'}</Text>
+        </View>}
+    </SafeAreaView>
+  );
 }
