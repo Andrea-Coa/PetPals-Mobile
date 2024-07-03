9a5aabd8710c40516d843f1a350f55799233f300
Elvis Alejandro Lopez Gambini <elvis_lg06@hotmail.com>
2024-07-03 12:33:18 -0500

    pequeño avance

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ import {jwtDecode} from 'jwt-decode';
 import * as SecureStore from 'expo-secure-store';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://172.20.10.2:8080'; //Cambiar por tu ip
+const API_URL = 'http://192.168.1.238:8080'; //Cambiar por tu ip
 
 export const getRoleBasedOnToken = async () => {
   try {
